91a0d3c447ee1823f274ddc1990aabddcc2397be
Gilmer Yturri <king_y4@hotmail.com>
2016-09-15 12:46:02 -0500

    correccion

--- hello/forms.py
+++ hello/forms.py
@@ -3,7 +3,7 @@ from django.forms import ModelForm
 
 from .models import Caidos
 
-class NpacienteForm(forms.ModelForm):
+class CaidoForm(forms.ModelForm):
 	class Meta:
 		model = Caidos
 		exclude = ("id","reg",)
