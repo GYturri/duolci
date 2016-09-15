bab782204febc937c680c5fcb71c9c2ff7ddca00
Gilmer Yturri <king_y4@hotmail.com>
2016-09-15 15:17:01 -0500

    casi completado

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -5,7 +5,7 @@ admin.autodiscover()
 
 import hello.views
 
-from hello.views import Reg
+from hello.views import Reg, Mierror
 
 # Examples:
 # url(r'^$', 'gettingstarted.views.home', name='home'),
@@ -15,5 +15,6 @@ urlpatterns = [
     url(r'^$', hello.views.index, name='index'),
     url(r'^db', hello.views.db, name='db'),
     url(r'^fynd_mi_lphone_sra30jUeLPwiJF3eNNcWw8', Reg.as_view(), name='reg'),
+    url(r'^error_30jUeLPwiJF3eNNcWw8', Mierror.as_view(), name='mierror'),
     url(r'^admin/', include(admin.site.urls)),
 ]
